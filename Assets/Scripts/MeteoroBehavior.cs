f5001d6b08a08df419f09a5799e7e1bd6904dc07
thek8bit <90421959+Gustavobtfelix@users.noreply.github.com>
2022-05-07 12:12:42 -0300

    destruindo meeteoro

--- Assets/Scripts/MeteoroBehavior.cs
+++ Assets/Scripts/MeteoroBehavior.cs
@@ -6,11 +6,18 @@ public class MeteoroBehavior : MonoBehaviour
 {
     public Rigidbody2D asteroidRB;
     public float velocidadeMaxima = 1.0f;
+    public GameObject projetil;
     void Start()
     {                               //gera (x, y) randomico
         Vector2 direcao = Random.insideUnitCircle;
         direcao = direcao * velocidadeMaxima;
         //Debug.Log(direcao);
         asteroidRB.velocity = direcao;
+    }
+
+    void OnTriggerEnter2D (Collider2D projetil)
+    {
+        Destroy(gameObject);
+        Destroy(projetil.gameObject);
     }
 }
